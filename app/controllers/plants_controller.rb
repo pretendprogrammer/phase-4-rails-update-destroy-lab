d8014e06e95352e50e1faf1962bc45715c1c39ae
Yosef <shmuli5770@gmail.com>
2021-06-07 17:51:02 -0400

    commit tree b117755c47366162c3f01538bbfbfcbec608b220

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,8 +8,8 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
+    plant = find_plant
+    render json: find_plant
   end
 
   # POST /plants
@@ -18,7 +18,22 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant, status: 200 
+  end
+
+  def destroy
+    find_plant.destroy
+    render json: {}
+  end
+
   private
+
+  def find_plant
+    Plant.find_by(id: params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
